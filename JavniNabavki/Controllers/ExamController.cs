b13643ca42d7253bcbf6ad65020bb8c3e1fba582
Teodora <siljanoska.teodora@uklo.edu.mk>
2023-02-21 20:43:06 +0100

    Changes required

--- JavniNabavki/Controllers/ExamController.cs
+++ JavniNabavki/Controllers/ExamController.cs
@@ -27,13 +27,49 @@ namespace JavniNabavki.Controllers
             return await _examRepository.Get(id);
         }
 
-        [HttpPost]
+        /* [HttpPost]
         public async Task<ActionResult<Exam>> PostExams([FromBody] Exam exam)
         {
             var newExam = await _examRepository.Create(exam);
             return CreatedAtAction(nameof(GetExams), new { id = newExam.Id }, newExam);
         }
+        */
 
+        [HttpPost]
+         
+        public async Task<ActionResult<Exam>> PostExams([FromBody] Exam exam)
+        {
+            var exams = await _examRepository.Get();
+
+
+
+            var existingExam = exams.Where(x =>
+            (x.Pocetok != null ? x.Pocetok.Value.Date == exam.Pocetok.Value.Date : x.Pocetok == exam.Pocetok)
+            && (x.Kraj != null ? x.Kraj.Value.Date == exam.Kraj.Value.Date : x.Kraj == exam.Pocetok)
+        ).FirstOrDefault();
+
+            if (existingExam != null)
+            {
+                return Ok("Obuka so istiot datum na pocetok i datum na kraj vekje postoi.");
+            }
+
+          
+
+            var existingExam_datum = exams.Where(y =>
+                   (y.Datum != null ? y.Datum.Value.Date == exam.Datum.Value.Date : y.Datum == exam.Datum)
+
+               ).FirstOrDefault();
+
+                if (existingExam_datum != null)
+                {
+                    return Ok("Obuka so istiot datum vekje postoi.");
+                }
+            
+
+
+            var newExam = await _examRepository.Create(exam);
+            return CreatedAtAction(nameof(GetExams), new { id = newExam.Id }, newExam);
+        }
         [HttpPut]
         public async Task<ActionResult> PutExams(int id, [FromBody] Exam exam)
         {
